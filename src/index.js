23b2ddf96158c147e28d116397dff75958ac6dd8
Inga Grigorieva <ingagrigorieva0402@gmail.com>
2021-01-15 19:58:28 +0200

    Внесены изменения в index.html

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './css/styles.css';
 import 'material-design-icons/iconfont/material-icons.css';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-import '../js/script.js';
+import './js/script.js';
